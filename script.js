55920accad2739f390c2f12445f00bcbfbad07c7
JefO <jef.ogu@gmail.com>
2023-04-11 19:53:47 -0400

    create random city functiuon
    random city generastor

---
+++ script.js
@@ -0,0 +1,21 @@
+function getCity() {
+  var search = 'https://api.teleport.org/api/urban_areas';
+
+  fetch(search)
+  .then (function (response) {
+    if(!response) {
+      throw response.json();
+    }
+    return response.json();
+  })
+  .then (function (data) {
+    console.log(data)
+    var cityList = data._links['ua:item'];
+    var random = Math.floor(Math.random()* cityList.length)
+    console.log(cityList)
+    console.log(random)
+    console.log(cityList[random])
+  })
+}
+
+getCity();
