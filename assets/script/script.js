08fe2e2d5747ac189006c11617dd78f43adf637f
JefO <jef.ogu@gmail.com>
2023-04-13 20:17:51 -0400

    Update script.js
    added functions to get city details and city image

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,9 @@
+var h1Name = document.getElementById('city-name');
+var imgCity = document.getElementById('city-image');
+
+// get random city from api
 function getCity() {
   var search = 'https://api.teleport.org/api/urban_areas';
-
   fetch(search)
   .then (function (response) {
     if(!response) {
@@ -9,13 +12,47 @@ function getCity() {
     return response.json();
   })
   .then (function (data) {
-    console.log(data)
     var cityList = data._links['ua:item'];
     var random = Math.floor(Math.random()* cityList.length)
-    console.log(cityList)
-    console.log(random)
     console.log(cityList[random])
+    var cityDetails = cityList[random].href;
+    var cityName = cityList[random].name;
+    h1Name.textContent = cityName;
+    // getCityDetails(city)
+    getCityImage(cityDetails)
   })
 }
 
+// get City Details of select city
+function getCityDetails(city) {
+  city +='details';
+  fetch(city)
+  .then (function (response) {
+    if(!response) {
+      throw response.json();
+    }
+    return response.json();
+  })
+  .then (function (data) {
+    // console.log(data)
+  })
+}
+
+// get city image of select city
+function getCityImage(city) {
+  city +='images';
+  fetch(city)
+  .then (function (response) {
+    if(!response) {
+      throw response.json();
+    }
+    return response.json();
+  })
+  .then (function (data) {
+    // console.log(data)
+    console.log(data.photos[0].image.web)
+    var cityImage = data.photos[0].image.web;
+    imgCity.src = cityImage;
+  })
+}
 getCity();
