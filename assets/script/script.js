cb0ad63aaa43c2ff1ebac6cfaf2d258690737a3c
maeahmad <122010497+maeahmad@users.noreply.github.com>
2023-04-17 21:14:22 -0400

    added 5-Day forecast information

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,17 @@
 var cityInputEl = document.querySelector("#city-input")
 var searchButtonEl = document.querySelector("#search-btn")
+var searchValue = cityInputEl.value
 
 searchButtonEl.addEventListener("click", function () {
     var searchValue = cityInputEl.value
     console.log(searchValue)
 
     getWeatherInfo(searchValue)
-
+    getForecast(searchValue)
 })
 
 
-// get weather information
+// current weather information
 var apiKey = 'c36e92a6ba7d753715bfc90e32631c4e';
 var getWeatherInfo = function (searchValue) {
     var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${apiKey}&units=imperial`;
@@ -56,3 +57,41 @@ var getWeatherInfo = function (searchValue) {
     })
 }
 
+// forecast information
+var getForecast = function (searchValue) {
+    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=${apiKey}&units=imperial`;
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+
+
+                    for (var i = 0; i < 5; i++) {
+                        var forecast = data.list[i * 8];
+
+                        var temp = document.createElement("p");
+                        document.querySelector(`.fiveDay-temp${i + 1}`).innerHTML = "";
+                        temp.textContent = forecast.main.temp;
+                        document.querySelector(`.fiveDay-temp${i + 1}`).append(temp);
+
+                        var humidity = document.createElement("p");
+                        document.querySelector(`.fiveDay-humid${i + 1}`).innerHTML = "";
+                        humidity.textContent = forecast.main.humidity;
+                        document.querySelector(`.fiveDay-humid${i + 1}`).append(humidity);
+
+                        var wind = document.createElement("p");
+                        document.querySelector(`.fiveDay-wind${i + 1}`).innerHTML = "";
+                        wind.textContent = forecast.wind.speed;
+                        document.querySelector(`.fiveDay-wind${i + 1}`).append(wind);
+
+                        var date = document.createElement("p");
+                        document.querySelector(`#date${i + 1}`).innerHTML = "";
+                        date.textContent = forecast.dt_txt;
+                        document.querySelector(`#date${i + 1}`).append(date);
+                    }
+                });
+            }
+        });
+};
